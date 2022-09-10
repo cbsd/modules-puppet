099e2ef7731ca20503aa7b166d37887b398d281b
olevole <olevole@srv-01.convectix.com>
2022-09-11 01:35:03 +0300

    wip

--- modules/public/profiles/files/powerdns/curl-create-record.sh
+++ modules/public/profiles/files/powerdns/curl-create-record.sh
@@ -6,3 +6,4 @@ progdir="${0%/*}" # Program directory
 . ${progdir}/config
 
 curl -s -H "X-API-Key: ${API_KEY}" -X PATCH -d '{"rrsets":[{"name":"mytest.example.org.","type":"TXT","ttl":3600,"changetype":"REPLACE","records":[{"content":"\"this is a test\"","set_ptr":false,"disabled":false}]}]}' http://127.0.0.1:8081/api/v1/servers/localhost/zones/example.org
+curl -s -H "X-API-Key: ${API_KEY}" -X PATCH -d '{"rrsets":[{"name":"mytest.example.org.","type":"A","ttl":3600,"changetype":"REPLACE","records":[{"content":"127.0.0.1","set_ptr":false,"disabled":false}]}]}' http://127.0.0.1:8081/api/v1/servers/localhost/zones/example.org
